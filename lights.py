e02776225efc411e17f5e8ca325396d32e022cd1
Jared Hobbs <jared@pyhacker.com>
2017-04-25 12:18:28 -0700

    make button toggle party lights on/off

--- lights.py
+++ lights.py
@@ -11,59 +11,78 @@ from phue import Bridge
 with open('config.json', 'r') as f:
     config = json.load(f)
 dash_config = config['Dash']
-hue_config = config['Hue']
-b = Bridge(hue_config['bridge-ip'])
-b.connect()
 
 # After party time, how many seconds before we allow ourselves to activate
 # the lights again
 DEBOUNCE_INTERVAL = config['Debounce']
 
-# Setup lights
-light_group = hue_config['light-group']
-group_name = hue_config['light-group-name']
-b.create_group(group_name, light_group)
-base_light_state = {'bri': 120, 'sat': 0, 'transitiontime': 10, 'on': True}
 
+class PartyTime(object):
+    def __init__(self):
+        hue_config = config['Hue']
+        self.bridge = Bridge(hue_config['bridge-ip'])
+        self.bridge.connect()
 
-def baseLights():
-    b.set_group(group_name, base_light_state)
-    time.sleep(1)
+        # Setup lights
+        self.light_group = hue_config['light-group']
+        self.group_name = hue_config['light-group-name']
+        self.bridge.create_group(self.group_name, self.light_group)
+        self.base_light_state = {
+            'bri': 120,
+            'sat': 0,
+            'transitiontime': 10,
+            'on': True,
+        }
 
+        self.enabled = False
+        self.base_lights()
 
-def huePoint(color):
-    """Splits the spectrum up by the number of lights in the light group to get
-    an evenly spaced rainbow of colors
-    """
-    color = int(color % len(light_group))
-    hue = 65535 / len(light_group)
-    hue = hue * color
-    return hue
+    def base_lights(self):
+        self.enabled = False
+        self.bridge.set_group(self.group_name, self.base_light_state)
+        time.sleep(1)
 
+    def hue_point(self, color):
+        """
+        Splits the spectrum up by the number of lights in the light group to
+        get an evenly spaced rainbow of colors
+        """
+        color = int(color % len(self.light_group))
+        hue = 65535 / len(self.light_group)
+        hue = hue * color
+        return hue
 
-def partyLights(cycles):
-    """Sets a random light, the next step in the rainbow,
-    defined using the huePoint function
-    """
-    cycles = cycles * 16
-    random.shuffle(light_group)
-    params = {'sat': 254, 'bri': 254, 'transitiontime': 1}
-    for i in range(cycles):
-        params['hue'] = huePoint(i)
-        b.set_light(random.choice(light_group), params)
-        time.sleep(0.05)
-    # reset to base state
-    base = dict(base_light_state)
-    base['transitiontime'] = 3
-    for light in light_group:
-        b.set_light(light, base)
-        time.sleep(0.2)
-    baseLights()
+    def start(self):
+        """
+        Sets a random light, the next step in the rainbow, defined using the
+        hue_point method
+        """
+        self.enabled = True
+        random.shuffle(self.light_group)
+        params = {'sat': 254, 'bri': 254, 'transitiontime': 1}
+        i = 0
+        while self.enabled:
+            params['hue'] = self.hue_point(i)
+            self.bridge.set_light(random.choice(self.light_group), params)
+            time.sleep(0.05)
+            i += 1
 
+    def stop(self):
+        # reset to base state
+        self.enabled = False
+        base = dict(self.base_light_state)
+        base['transitiontime'] = 3
+        for light in self.light_group:
+            self.bridge.set_light(light, base)
+            time.sleep(0.2)
+        self.base_lights()
 
-def partyTime():
-    print "Button clicked"
-    partyLights(10)
+    def toggle(self):
+        if not self.enabled:
+            self.start()
+        else:
+            self.stop()
+
 
 # Ignore SIGCHLD
 # This will prevent zombies
@@ -80,7 +99,7 @@ cmd = 'tcpdump -l -K -q -i ButtonMonitor -n -s 256'
 proc = subprocess.Popen(cmd.split(), close_fds=True,
                         bufsize=0, stdout=subprocess.PIPE)
 
-baseLights()
+party_time = PartyTime()
 last_played = 0
 while True:
     line = proc.stdout.readline()
@@ -94,4 +113,4 @@ while True:
                 last_played = now
                 sys.stdout.write(line)
                 sys.stdout.flush()
-                partyTime()
+                party_time.toggle()
